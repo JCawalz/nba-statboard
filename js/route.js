d34811d2eb47324fd32aaa4c35d3191687df3808
KimRosales <khrosales18@gmail.com>
2017-06-16 13:03:37 +0800

    Updated from the start,field goal 3pt percentage and fantasy points remains

--- js/route.js
+++ js/route.js
@@ -9,16 +9,6 @@ app.config(function($stateProvider, $urlRouterProvider){
       url:'/',
       templateUrl: 'templates/homePage.html'
     })
-    .state('fieldGoalPCT',{
-          url:'/fieldGoalPCT',
-          templateUrl: 'templates/fieldGoalPCT.html',
-          params:{nameResult: null, valueResult: null}
-    })
-    .state('fieldGoal3P',{
-          url:'/fieldGoal3P',
-          templateUrl: 'templates/fieldGoal3P.html',
-          params:{nameResult: null, valueResult: null}
-    })
 
     .state('fg3percent',{
        url:'/fg3percent',
@@ -29,31 +19,6 @@ app.config(function($stateProvider, $urlRouterProvider){
            url:'/ffpg',
            templateUrl: 'templates/FPPage.html',
            params:{nameResult: null, valueResult: null}
-     })
-    .state('points',{
-      url:'/top_5_players_in_points',
-      templateUrl: 'templates/ptsPage.html',
-      params:{nameResult_PTS: null, valueResult_PTS: null, _flag_PTS: null}
-    })
-    .state('rebounds',{
-      url:'/top_5_players_in_rebounds',
-      templateUrl: 'templates/rebPage.html',
-      params:{nameResult_REB: null, valueResult_REB: null, _flag_REB: null}
-    })
-    .state('assists',{
-      url:'/top_5_players_in_assists',
-      templateUrl: 'templates/astPage.html',
-      params:{nameResult_AST: null, valueResult_AST: null, _flag_AST: null}
-    })
-    .state('stealsPage',{
-      url:'/stealsPage',
-      templateUrl: 'templates/stealsPage.html',
-      params:{nameResult: null, valueResult: null, _flag: null}
-    })
-    .state('blocksPage',{
-      url:'/blocksPage',
-      templateUrl: 'templates/blocksPage.html',
-      params:{nameResult: null, valueResult: null, _flag: null}
-    });
+     });
 
 });
