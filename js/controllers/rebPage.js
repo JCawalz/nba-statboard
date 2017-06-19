5466fb3532d7c4311fc8a1db42dd27438c19c678
JCawalz <jcawaling.jc@gmail.com>
2017-06-19 15:06:28 +0800

    Updated features' js files and routing

--- js/controllers/rebPage.js
+++ js/controllers/rebPage.js
@@ -1,7 +1,6 @@
 app.controller('rebCtrl', ['$scope', '$state', '$stateParams', function($scope, $state, $stateParams) {
 	//REBOUNDS CONTROLLER
 	$scope.rank = ['1st', '2nd', '3rd', '4th', '5th'];
-	$scope.init = $stateParams._flag_REB;
 	$scope.dataName = $stateParams.nameResult_REB;
 	$scope.dataValue = $stateParams.valueResult_REB;
 
